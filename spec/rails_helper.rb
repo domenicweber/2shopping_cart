c41efffcc634820301040d5dab70b005dd1c60e1
Domenic Weber <domenic.weber@gmail.com>
2015-12-15 08:00:25 -0700

    WIP spec files

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,16 +8,16 @@ require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
-# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
+# spec/support/ and its subdirectories. Files matching `spec/**/*creating_products_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
-# in _spec.rb will both be required and run as specs, causing the specs to be
+# in creating_products_spec.rb will both be required and run as specs, causing the specs to be
 # run twice. It is recommended that you do not name files matching this glob to
-# end with _spec.rb. You can configure this pattern with the --pattern
+# end with creating_products_spec.rb. You can configure this pattern with the --pattern
 # option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
 #
 # The following line is provided for convenience purposes. It has the downside
 # of increasing the boot-up time by auto-requiring all files in the support
-# directory. Alternatively, in the individual `*_spec.rb` files, manually
+# directory. Alternatively, in the individual `*creating_products_spec.rb` files, manually
 # require only the support files necessary.
 #
 # Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
